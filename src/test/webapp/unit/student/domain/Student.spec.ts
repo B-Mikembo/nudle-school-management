5f5289fef5f4b2e9d51577447c319bb2032047ab
Brice Mikembo <bmikembo@etud.u-pem.fr>
2026-07-17 14:49:00 +0200

    feat: last name is required

--- src/test/webapp/unit/student/domain/Student.spec.ts
+++ src/test/webapp/unit/student/domain/Student.spec.ts
@@ -2,14 +2,18 @@ import { Student } from '@/student/domain/Student';
 
 describe('Student', () => {
   it('should throw error message when username has under 3 characters long', () => {
-    expect(() => new Student('Al', 'Aly')).toThrow('Username must be at least 3 characters long!');
+    expect(() => new Student('Al', 'Aly', 'Dupont')).toThrow('Username must be at least 3 characters long!');
   });
 
   it('should throw error message when username has greater than 20 characters long', () => {
-    expect(() => new Student('aaaaaaaaaaaaaaaaaaaaa', 'ahaha')).toThrow('Username must be at most 20 characters long!');
+    expect(() => new Student('aaaaaaaaaaaaaaaaaaaaa', 'ahaha', 'Dupont')).toThrow('Username must be at most 20 characters long!');
   });
 
   it('should throw error when name is blank', () => {
-    expect(() => new Student('Tony', ' ')).toThrow('First name is required');
+    expect(() => new Student('Tony', ' ', 'Dupont')).toThrow('First name is required');
+  });
+
+  it('should throw error when lastname is blank', () => {
+    expect(() => new Student('Tony', 'Anthony', ' ')).toThrow('Last name is required');
   });
 });
