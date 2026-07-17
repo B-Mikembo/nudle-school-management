fdb96922f046bfc65acbe9858b43252a30775f3b
Brice Mikembo <bmikembo@etud.u-pem.fr>
2026-07-17 14:41:21 +0200

    feat: first name is required

--- src/main/webapp/app/student/domain/Student.ts
+++ src/main/webapp/app/student/domain/Student.ts
@@ -1,6 +1,7 @@
 export class Student {
-  constructor(username: string) {
+  constructor(username: string, name: string) {
     if (username.length < 3) throw Error('Username must be at least 3 characters long!');
     if (username.length > 20) throw Error('Username must be at most 20 characters long!');
+    if (name.trim().length === 0) throw Error('First name is required');
   }
 }
--- src/test/webapp/unit/student/domain/Student.spec.ts
+++ src/test/webapp/unit/student/domain/Student.spec.ts
@@ -2,10 +2,14 @@ import { Student } from '@/student/domain/Student';
 
 describe('Student', () => {
   it('should throw error message when username has under 3 characters long', () => {
-    expect(() => new Student('Al')).toThrow('Username must be at least 3 characters long!');
+    expect(() => new Student('Al', 'Aly')).toThrow('Username must be at least 3 characters long!');
   });
 
   it('should throw error message when username has greater than 20 characters long', () => {
-    expect(() => new Student('aaaaaaaaaaaaaaaaaaaaa')).toThrow('Username must be at most 20 characters long!');
+    expect(() => new Student('aaaaaaaaaaaaaaaaaaaaa', 'ahaha')).toThrow('Username must be at most 20 characters long!');
+  });
+
+  it('should throw error when name is blank', () => {
+    expect(() => new Student('Tony', ' ')).toThrow('First name is required');
   });
 });
