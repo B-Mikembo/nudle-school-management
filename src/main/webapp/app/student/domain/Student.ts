27552fc859a16e58db313cb2310f2ba3a85bf32f
Brice Mikembo <bmikembo@etud.u-pem.fr>
2026-07-17 14:52:36 +0200

    feat: address is required

--- src/main/webapp/app/student/domain/Student.ts
+++ src/main/webapp/app/student/domain/Student.ts
@@ -1,8 +1,9 @@
 export class Student {
-  constructor(username: string, name: string, lastname: string) {
+  constructor(username: string, name: string, lastname: string, address: string) {
     if (username.length < 3) throw Error('Username must be at least 3 characters long!');
     if (username.length > 20) throw Error('Username must be at most 20 characters long!');
     if (name.trim().length === 0) throw Error('First name is required');
     if (lastname.trim().length === 0) throw Error('Last name is required');
+    if (address.trim().length === 0) throw Error('Address is required');
   }
 }
